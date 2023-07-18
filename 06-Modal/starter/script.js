308016873141a48dd32bb0995e78a0593e6cb218
martinwprior <martinwprior@gmail.com>
2023-07-18 15:08:39 +0800

    completed pig game

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+// add event listeners
+
+// select multiple nodes for the 3 buttons. on click show modal
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// when the X is clicked on modal, close
+btnCloseModal.addEventListener('click', closeModal);
+
+// when the overlay is clicked, close
+overlay.addEventListener('click', closeModal);
+
+// when Esc key pressed
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
